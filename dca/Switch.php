2b5dc4aaf79c5988f0fba58e625ebbb4b023824e
Peter van Es <vanes.peter@gmail.com>
2013-12-15 15:27:59 +0100

    idroom changed to sensor_id

---
+++ dca/Switch.php
@@ -0,0 +1,233 @@
+<?php
+
+// <copyright> Copyright (c) 2012-2013 All Rights Reserved,
+// Escurio BV
+// http://www.escurio.com/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Peter van Es</author>
+// <version>1.0</version>
+// <email>vanesp@escurio.com</email>
+// <date>2013-12-15</date>
+// <summary>dca/Switch.php defines the Switch table</summary>
+
+// version 1.0
+
+
+/*
+CREATE TABLE `Switch` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL COMMENT 'Location.id',
+  `tstamp` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL COMMENT 'tl_user.id owner',
+  `idswitch` varchar(15) DEFAULT '',
+  `sensor_id` int(10) DEFAULT NULL COMMENT 'roomnode number',
+  `description` varchar(80) DEFAULT NULL COMMENT 'description',
+  `comments` varchar(4096) DEFAULT NULL COMMENT 'Comments time_on installation, remarks',
+  `strategy` varchar(45) DEFAULT NULL,
+  `command` varchar(80) DEFAULT NULL,
+  `kaku` varchar(8) DEFAULT NULL,
+  `newkaku` varchar(20) DEFAULT NULL,
+  `time_on` varchar(8) DEFAULT NULL,
+  `time_off` varchar(8) DEFAULT NULL,
+  `state` varchar(8) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL COMMENT 'Minutes',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores sensor machine details';*/
+
+if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+// Table Switch
+$GLOBALS['TL_DCA']['Switch'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'		=> 'Table',
+		'ctable' => array('Actionlog'),
+		'notEditable'		=> false,
+        'enableVersioning'  => true,
+		'closed' => false 			// implicit
+	),
+
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+				'mode'                    => 0,
+				'fields'                  => array('id'), // Elements sorted time_on id
+				'headerFields'	  	      => array('id', 'description'),
+				'panelLayout'             => 'filter,sort; search,limit',
+				'flag'                    => 1
+		),
+		'label' => array
+		(
+				'fields'                  => array('id', 'pid', 'idswitch',  'description', 'kaku'), // Fields shown in the panel
+				'showColumns'             => true,
+				'format'                  => '%s</td><td class="tl_file_list"><a href="contao/main.php?do=Customers&table=Location&id=%s">%s</a></td><td class="tl_file_list">%s'
+		),
+		'operations' => array
+		(
+			'editheader' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['Switch']['editheader'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif',
+//				'attributes'          => 'class="edit-header"'
+				'attributes'          => 'class="contextmenu"'
+			),
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['Switch']['edit'],
+				'href'                => 'table=Actionlog',
+				'icon'                => 'header.gif',
+				'attributes'          => 'class="contextmenu"'
+			),
+			'copy' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['Switch']['copy'],
+				'href'                => 'act=copy',
+				'icon'                => 'copy.gif',
+				'attributes'          => 'class="contextmenu"'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['Switch']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['Switch']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif'
+			)
+		)	// operations
+	),		// list
+
+
+// Palettes
+'palettes' => array
+(
+	// palettes settings
+	'default'               => '{Switch_legend}, pid, sensor_id, description, uid, comments;
+					{activity_legend}, strategy, command, kaku, newkaku, time_on, time_off, duration'
+),
+
+
+// Fields
+'fields' => array
+(
+	'pid' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['pid'],
+		'search'                  => false,
+		'sorting'				  => true,
+		'flag'					  => 11,
+		'filter'				  => true,
+		'exclude'                 => true,
+		'inputType'               => 'select',
+		'foreignKey'	     	  => 'Location.name', 
+		'eval'                    => array('mandatory'=>true, 'readonly'=> false, 'doNotCopy'=>true, 'includeBlankOption'=>true, 'tl_class'=>'w50')
+	),
+	'sensor_id' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['sensor_id'],
+		'search'                  => true,
+		'sorting'		          => true,
+		'inputType'               => 'select',
+		'foreignKey'              => 'Sensor.location',
+		'eval'                    => array('mandatory'=>true, 'rgxp'=>'digit', 'maxlength'=>12, 'tl_class'=>'w50')
+	),
+
+	'description' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['description'],
+		'search'                  => true,
+		'sorting'				  => true,
+		'filter'				  => true,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>true, 'minLength'=>3, 'maxlength'=>255, 'tl_class'=>'w50')
+	),
+	'uid' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['uid'],
+		'inputType'               => 'select',
+		'foreignKey'	     	  => 'tl_user.name', 
+		'eval'                    => array('mandatory'=>true, 'doNotCopy'=>true, 'includeBlankOption'=>true, 'tl_class'=>'w50')
+	),
+	'comments' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['comments'],
+		'inputType'               => 'textarea',
+		'eval'                    => array('tl_class'=>'clr',  'rte'=>'tinyMCE', 'allowHTML'=>true, 'mandatory'=>false, 'maxlength'=>4096)
+	),
+	'strategy' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['strategy'],
+		'search'                  => false,
+		'inputType'               => 'select',
+		'options'				  => array('motion', 'sun', 'evening', 'time', 'light', 'simulate', 'event'),
+		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50')
+	),
+	'command' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['command'],
+		'search'                  => false,
+		'sorting'				  => false,
+		'filter'				  => false,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>true, 'minLength'=>1, 'maxlength'=>80, 'tl_class'=>'w50')
+	),
+	'kaku' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['kaku'],
+		'search'                  => false,
+		'sorting'				  => false,
+		'filter'				  => false,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>false, 'minLength'=>1, 'maxlength'=>15, 'tl_class'=>'w50')
+	),
+	'newkaku' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['newkaku'],
+		'search'                  => false,
+		'default'				  => '', 
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>false, 'minLength'=>1, 'maxlength'=>15, 'tl_class'=>'w50')
+	),
+	'time_on' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['time_on'],
+		'search'                  => false,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>false, 'rgxp'=>'time', 'minLength'=>1, 'maxlength'=>8, 'tl_class'=>'w50')
+	),
+	'time_off' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['time_off'],
+		'search'                  => false,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>false, 'rgxp'=>'time', 'minLength'=>1, 'maxlength'=>8, 'tl_class'=>'w50')
+	),
+	'duration' => array
+	(
+		'label'                   => &$GLOBALS['TL_LANG']['Switch']['duration'],
+		'search'                  => false,
+		'inputType'               => 'text',
+		'eval'                    => array('mandatory'=>false, 'rgxp'=>'digit', 'minLength'=>1, 'maxlength'=>8, 'tl_class'=>'w50')
+	),
+) // fields
+
+);
+
+
+
+?>
---
+++ languages/en/Switch.php
@@ -0,0 +1,47 @@
+<?php
+
+// <copyright> Copyright (c) 2012-2013 All Rights Reserved,
+// Escurio BV
+// http://www.escurio.com/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Peter van Es</author>
+// <version>1.0</version>
+// <email>vanesp@escurio.com</email>
+// <date>2013-12-15</date>
+// <summary>languages/en/Switch.php defines the Switch language</summary>
+
+// Buttons
+$GLOBALS['TL_LANG']['Switch']['new']= array('New Switch', 'Create a new Switch');
+$GLOBALS['TL_LANG']['Switch']['edit']= array('Edit', 'Edit Switch Actionlog');
+$GLOBALS['TL_LANG']['Switch']['editheader']= array('Edit', 'Edit Switch %s');
+$GLOBALS['TL_LANG']['Switch']['delete']= array('Delete', 'Delete Switch %s');
+$GLOBALS['TL_LANG']['Switch']['copy']= array('Copy', 'Copy Switch %s');
+$GLOBALS['TL_LANG']['Switch']['show']= array('Show', 'Show Switch %s');
+
+// Legends
+$GLOBALS['TL_LANG']['Switch']['Switch_legend']= 'Switch';
+$GLOBALS['TL_LANG']['Switch']['activity_legend']= 'Activation details';
+$GLOBALS['TL_LANG']['Switch']['details_legend']= 'Switch details';
+$GLOBALS['TL_LANG']['Switch']['limits_legend']= 'Alert limits';
+
+// Fields
+$GLOBALS['TL_LANG']['Switch']['pid']= array('Location id', 'Select the location');
+$GLOBALS['TL_LANG']['Switch']['sensor_id']= array('Sensor id', 'Select the id of the sensor associated with this switch');
+$GLOBALS['TL_LANG']['Switch']['uid']= array('Owner', 'Select the user responsible for this switch');
+$GLOBALS['TL_LANG']['Switch']['description']= array('Description', 'Name of the switch');
+$GLOBALS['TL_LANG']['Switch']['comments']= array('Comments... Not for actionlog items!', 'Enter any special remarks for this switch, no action log items!');
+$GLOBALS['TL_LANG']['Switch']['strategy']= array('Strategy', 'Determine how this switch should be activated (time_on motion, or rising or setting of the sun, evening only, time on and off, light levels, or simulated presence.');
+$GLOBALS['TL_LANG']['Switch']['command']= array('Command', 'Enter the command string to activate this switch, e.g. SendKAKU E1, or SendNewKAKU 2,. Do include the trailing comma');
+$GLOBALS['TL_LANG']['Switch']['kaku']= array('Kaku', 'Klik-Aan-Klik-Uit identifier, e.g. E1');
+$GLOBALS['TL_LANG']['Switch']['newkaku']= array('NewKaku', 'New Klik-Aan-Klik-Uit identifier, e.g. 0x285E880');
+$GLOBALS['TL_LANG']['Switch']['time_on']= array('Time on', 'Time at which to switch on');
+$GLOBALS['TL_LANG']['Switch']['time_off']= array('Time off', 'Time at which to switch off');
+$GLOBALS['TL_LANG']['Switch']['duration']= array('Duration', 'Duration (in minutes) activation after an event');
+
+?>
